c1d144da6432160376c4936983d41f9f7c0513b9
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-01-30 21:27:55 +0700

    Ví dụ đầu tiên
    Demo

---
+++ HelloWorld/src/Java/XinChao.java
@@ -0,0 +1,13 @@
+package Java;
+public class XinChao {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("Cac phep toan");
+        int a = 2;
+        int b = 3;
+        System.out.println("Tong: "+ (a + b));
+        System.out.println("Tich: "+ (a * b));
+        System.out.println("Hieu: "+ (b - a));
+        System.out.println("Thuong: "+ (b / a));
+	}
+}
