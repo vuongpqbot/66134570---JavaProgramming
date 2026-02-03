f5a68dd42c6f5b1dfe8aaf8af5e9cac4fc7aadba
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-02-03 21:52:20 +0700

    Hoàn thiện BaiTap1

--- BaiTap1_Bien&PhepToan/src/BaiTap1.java
+++ BaiTap1_Bien&PhepToan/src/BaiTap1.java
@@ -1,9 +1,17 @@
-
 public class BaiTap1 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//1. Khai báo các biến
+		int a = 15;
+		int b = 4;
+		double c = 3.5;
+//2. Tính toán
+		int tong = a + b;
+		double thuong = a / c;
+		int PhanDu = a;
+// 3.In kết quả
+		System.out.println("Tổng: "+ tong);
+		System.out.println("Thương: "+ thuong);
+		System.out.println("Phần Dư: "+ PhanDu);
 	}
 
 }
