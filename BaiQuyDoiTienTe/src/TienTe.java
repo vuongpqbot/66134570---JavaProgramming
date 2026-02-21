32de347857399157a83a8d11bc66f868df0c5382
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-02-21 21:29:52 +0700

    QuyDoiTienTe

--- BaiQuyDoiTienTe/src/TienTe.java
+++ BaiQuyDoiTienTe/src/TienTe.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner;
 public class TienTe {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Scanner Nhap = new Scanner(System.in);
+        // Nhập tiền USD
+        System.out.print("Nhập số tiền USD: ");
+        double USD = Nhap.nextDouble();
+        // Nhập tiền EUR
+        System.out.print("Nhập số tiền EUR: ");
+        double EURO = Nhap.nextDouble();
+        // Quy đổi
+        double DoLa = USD * 23500;
+        double BangAnh = EURO * 27000;
+        // In kết quả
+        System.out.println("USD -> VND: " + String.format("%,.0f", DoLa) + " đ");
+        System.out.println("EURO -> VND: " + String.format("%,.0f", BangAnh) + " đ");
+        Nhap.close();
+    }
 }
