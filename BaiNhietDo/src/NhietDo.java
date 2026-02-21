58ff7712f0218d49dae56cbfe8d84b7acde222e0
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-02-21 20:35:53 +0700

    Nhiệt Độ

---
+++ BaiNhietDo/src/NhietDo.java
@@ -0,0 +1,9 @@
+public class NhietDo {
+	public static void main(String[] args) {
+		//Chuyển từ celsius sang độ fahrenheit
+		//Công thức F = (C * 9/5) + 32
+        double celsius = 25.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+	}
+}
