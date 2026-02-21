fde53a16d8a46cb8c37c2148965f54802bea0ee5
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-02-21 21:54:40 +0700

    Phương Trình Bậc 1

--- BaiPhuongTrinhBac1/src/PhuongTrinhBac1.java
+++ BaiPhuongTrinhBac1/src/PhuongTrinhBac1.java
@@ -1,9 +1,21 @@
-
+import java.util.Scanner;
 public class PhuongTrinhBac1 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner Nhap = new Scanner(System.in);
+		System.out.print("Nhập a: ");
+		double a = Nhap.nextDouble();
+		System.out.print("Nhập b: ");
+		double b = Nhap.nextDouble();
+		if(a == 0) {
+			if(b == 0) {
+				System.out.println("Phương trình vô số nghiệm");
+			}else {
+				System.out.println("Phương trình vô nghiệm");
+			}
+		}else {
+			double x = -b/a;
+			System.out.println("Nghiệm x = " + x);
+		}
+		Nhap.close();
 	}
-
 }
