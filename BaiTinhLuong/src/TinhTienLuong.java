3c6117d57c29d475c092cd0037c27456cd3e59b5
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-02-21 21:15:28 +0700

    Tính Tiền Lương

--- BaiTinhLuong/src/TinhTienLuong.java
+++ BaiTinhLuong/src/TinhTienLuong.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 public class TinhTienLuong {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner Nhap = new Scanner(System.in);
+        System.out.print("Nhập số giờ làm: ");
+        double GioLam = Nhap.nextDouble();
+        System.out.print("Nhập lương theo giờ: ");
+        double TienLuong = Nhap.nextDouble();
+        double LuongTong;
+        if(GioLam <= 40) {
+        	LuongTong = GioLam * TienLuong;
+        }else {
+        	LuongTong = 40 * TienLuong + (GioLam - 40) * TienLuong * 1.5;
+        }
+        System.out.print("Tổng lương: "+ String.format("%,.0f", LuongTong) + "đ");
+        Nhap.close();
 	}
-
 }
