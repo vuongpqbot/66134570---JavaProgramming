bef0d62aaa998fae929bb2678f04c76e04590607
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-03-10 21:45:41 +0700

    update Rectangle 1_3

--- Ex_Rectangle_1_3/src/Rectangle.java
+++ Ex_Rectangle_1_3/src/Rectangle.java
@@ -1,4 +1,38 @@
-
 public class Rectangle {
-
+   private float length = 1.0f;
+   private float width = 1.0f;
+   //constructor mặc định
+   public Rectangle() {
+   }
+   //constructor có tham số 
+   public Rectangle(float length, float width) {
+	   this.length = length;
+	   this.width = width;
+   }
+   //getter
+   public float getLength() {
+	   return length;
+   }
+   public float getWidth() {
+	   return width;
+   }
+   //setter
+   public void setLength(float length) {
+	   this.length = length;
+   }
+   public void setWidth(float width) {
+	   this.width = width;
+   }
+   //tính diện tích
+   public double getArea() {
+	   return length * width;
+   }
+   //tính chu vi
+   public double getCircumference() {
+	   return 2 * (length + width);
+   }
+   //toString
+   public String toString() {
+	   return "Rectangle[length = " + length + ", width = " + width + "]";
+   }
 }
--- Ex_Rectangle_1_3/src/TestRectangle.java
+++ Ex_Rectangle_1_3/src/TestRectangle.java
@@ -1,9 +1,17 @@
-
 public class TestRectangle {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Rectangle r1 = new Rectangle(1.2f, 3.4f);
+		System.out.println(r1);  //toString
+		Rectangle r2 = new Rectangle();
+		System.out.println(r2);
+		//chạy setter và getter
+		r1.setLength(5.6f);
+		r1.setWidth(7.8f);
+		System.out.println(r1);  //toString
+		System.out.println("Chiều dài là: " + r1.getLength());
+		System.out.println("Chiều rộng là: " + r1.getWidth());
+		//chạy diện tích và chu vi 
+		System.out.printf("Diện tích là: %.2f%n", r1.getArea());
+		System.out.printf("Chu vi là: %.2f%n", r1.getCircumference());
 	}
-
 }
