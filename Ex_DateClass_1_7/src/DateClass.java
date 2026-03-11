517c0228d42354c8592f39fb26bb1d9f8ecfb425
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-03-11 20:29:12 +0700

    update 1_7

--- Ex_DateClass_1_7/src/DateClass.java
+++ Ex_DateClass_1_7/src/DateClass.java
@@ -1,4 +1,39 @@
-
 public class DateClass {
-
+    private int day;
+    private int month;
+    private int year;
+    public DateClass(int day, int month, int year) {
+    	this.day = day;
+    	this.month = month;
+    	this.year = year;
+    }
+    //getter
+    public int getDay() {
+    	return day;
+    }
+    public int getMonth() {
+    	return month;
+    }
+    public int getYear() {
+    	return year;
+    }
+    //setter
+    public void setDay(int day) {
+    	this.day = day;
+    }
+    public void setMonth(int month) {
+    	this.month = month;
+    }
+    public void setYear(int year) {
+    	this.year = year;
+    }
+    public void setDate(int day, int month, int year) {
+    	this.day = day;
+    	this.month = month;
+    	this.year = year;
+    }
+    //toString
+    public String toString() {
+        return String.format("%02d/%02d/%04d", day, month, year);
+    }
 }
---
+++ Ex_DateClass_1_7/src/TestDateClass.java
@@ -0,0 +1,17 @@
+public class TestDateClass {
+	public static void main(String[] args) {
+			      DateClass d1 = new DateClass(1, 2, 2014);
+			      System.out.println(d1);  // toString()
+			      // Test Setters and Getters
+			      d1.setMonth(12);
+			      d1.setDay(9);
+			      d1.setYear(2099);
+			      System.out.println(d1);  // toString()
+			      System.out.println("Month: " + d1.getMonth());
+			      System.out.println("Day: " + d1.getDay());
+			      System.out.println("Year: " + d1.getYear());
+			      // Test setDate()
+			      d1.setDate(3, 4, 2016);
+			      System.out.println(d1);  // toString()
+	}
+}
