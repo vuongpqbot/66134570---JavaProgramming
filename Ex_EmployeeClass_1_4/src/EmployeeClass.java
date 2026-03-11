2744754425a0bc1fa324e0cf3625a3820e820704
vuongpqbot <vuong.pq.66cntt@ntu.edu.vn>
2026-03-11 17:23:35 +0700

    update 1_4

--- Ex_EmployeeClass_1_4/src/EmployeeClass.java
+++ Ex_EmployeeClass_1_4/src/EmployeeClass.java
@@ -1,4 +1,43 @@
-
 public class EmployeeClass {
-
+     private int id;
+     private String firstName;
+     private String lastName;
+     private int salary;
+     public EmployeeClass(int id, String firstName, String lastName, int salary) {
+    	 this.id= id;
+    	 this.firstName = firstName;
+    	 this.lastName = lastName;
+    	 this.salary = salary;
+    }
+     //getter
+     public int getId() {
+    	 return id;
+     }
+     public String getFirstName() {
+    	 return firstName;
+     }
+     public String getLastName() {
+    	 return lastName;
+     }
+     public String getName() {
+    	 return firstName + " " + lastName;
+     }
+     public int getSalary() {
+    	 return salary;
+     }
+     public int getAnnualSalary() {
+    	 return salary * 12;
+     }
+     //setter
+     public void setSalary(int salary) {
+    	 this.salary = salary;
+     }
+     public int raiseSalary(int percent) {
+    	 salary += salary * percent / 100;
+    	 return salary;
+     }
+     //toString
+     public String toString() {
+    	 return "Employee[id =" + id + ", name = " + getName() + ", salary = " + salary + "]";
+     }
 }
--- Ex_EmployeeClass_1_4/src/TestEmployeeClass.java
+++ Ex_EmployeeClass_1_4/src/TestEmployeeClass.java
@@ -1,9 +1,18 @@
-
 public class TestEmployeeClass {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+         EmployeeClass e1 = new EmployeeClass(8, "Peter", "Tan", 2500);
+         System.out.println(e1);  //toString
+         //Getter and Setter
+         e1.setSalary(999);
+         System.out.println(e1);
+         System.out.println("id là: " + e1.getId());
+         System.out.println("FirstName là: " + e1.getFirstName());
+         System.out.println("LastName là: "+ e1.getLastName());
+         System.out.println("Salary là: " + e1.getSalary());
+         System.out.println("Name là: " + e1.getName());
+         System.out.println("AnnualSalary là: " + e1.getAnnualSalary());
+         //test kiểm tra lương 
+         System.out.println(e1.raiseSalary(10));
+         System.out.println(e1);
 	}
-
 }
